d3b1765e995a53d545e05c536476dba7ff6afa37
tektechnologies <info@tektechnologies.com>
2023-01-30 14:50:25 -0600

    add 404 handler

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const express = require('express');
 require('dotenv').config();
 //3. require cors
 const cors = require('cors');
+const { response } = require('express');
 
 
 //4. use express
@@ -19,9 +20,12 @@ const PORT = process.env.PORT || 5005;
 
 
 
+//7. add 404 route handleing
+app.get('*', (req, res) => {
+  response.status(404).send('This route is lost in space!');
+});
 
 
 
-
-
+//6. add listen() to keep server up and running
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
