fd6fef672f6b4005977f1f79d6992a3465de20a3
tektechnologies <info@tektechnologies.com>
2023-01-30 14:47:03 -0600

    add using statements

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const express = require('express');
 require('dotenv').config();
 //3. require cors
 const cors = require('cors');
+
+
+//4. use express
+const app = express();
+//5. use cors
+app.use(cors());
+const PORT = process.env.PORT || 5005;
